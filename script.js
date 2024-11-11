6ac19bca89329a34cb0ff2441ccd581a40fb49d1
candyshopclick <gabryipogo@gmail.com>
2024-11-11 11:22:57 +0100

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-document.getElementById("suggestionForm").addEventListener("submit", function(event) {
-    event.preventDefault();
-    const suggestion = document.getElementById("suggestionInput").value;
-    alert(`Grazie! Il suggerimento "${suggestion}" è stato ricevuto (non viene salvato realmente).`);
-    document.getElementById("suggestionInput").value = "";
-});
